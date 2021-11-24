62e0a0e3673d77af5cfbbd43f2df66e2f5b5022f
AntonioMarcosN <antoniomarcosn6@gmail.com>
2021-11-23 21:14:54 -0300

    atualizamos o programa testeSenac fazendo o post

--- src/controller/automoveisController.js
+++ src/controller/automoveisController.js
@@ -1,11 +1,27 @@
 const automoveis = require("../models/automoveis.json");
+const fs = require("fs")
 
 const getAllAutomoveis = (req, res) =>{
     console.log(req.url);
     res.status(200).send(automoveis)
 }
+const createAutomoveis = (req, res) => {
+    const { id, carro, tipo, fabricante, cor, novo, motor } = req.body
+    automoveis.push({ id, carro, tipo, fabricante, cor, novo, motor })
+    fs.writeFile("./src/models/automoveis.json", JSON.stringify(automoveis), 'utf8', function (err) { // gravando novo filme no array de filmes
+        if (err) {
+            res.status(500).send({ message: err })
+        } else {
+            console.log("Arquivo atualizado com sucesso!")
+            const automoveisFound = automoveis.find(automoveis => automoveis.id == id) // recupero o filme que foi criei no array de filmes      
+            res.status(200).send(automoveisFound)
+        }
+    })
+}
 
 module.exports = {
+    //getAllAutomoveis,
+    createAutomoveis,
     getAllAutomoveis,
 }
 
--- src/routes/automoveisRoutes.js
+++ src/routes/automoveisRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require('../controller/automoveisController')
 
 router.get("/", controller.getAllAutomoveis);
-
+router.post("/", controller.createAutomoveis);
+//router.get("/:id", controller.getAutomoveis);
 
 module.exports = router;
