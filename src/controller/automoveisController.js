c31c4380737584d6cd78f559087c873c0987a0ae
AntonioMarcosN <antoniomarcosn6@gmail.com>
2021-11-26 21:13:02 -0300

    criamos no projeto novo controller e houter, o put

--- src/controller/automoveisController.js
+++ src/controller/automoveisController.js
@@ -71,10 +71,67 @@ const updateAutomoveis = (req, res) => {
     }
 } 
 
+const updatenovoStatus = (req, res) => {
+    try {
+        const automoveisId = req.params.id // pego a informação do id no parametro da requisição
+        const novo = req.body.novo // pego a informação de novo no corpo da requisição. Ele terá valor true ou false, dependendo do que tiver sido passado
+
+        const automoveisToUpdate = automoveis.find(automoveis => automoveis.id == automoveisId) // separo o automovel que irei mudar o status
+        const automoveisIndex = automoveis.indexOf(automoveisToUpdate) // identifico o índice do automovel no meu array
+
+        if (automoveisIndex >= 0) { // verifico se o automovel existe no array de automoveis
+            automoveisToUpdate.novo = novo //atualizo o objeto com o novo status informando se foi assistido ou não
+            automoveis.splice(automoveisIndex, 1, automoveisToUpdate) // removo o automovel pelo índice substituindo pelo novo
+        } else {
+            res.status(404).send({ message: "automovel não encontrado para informar se é novo ou não" })
+        }
+
+        fs.writeFile("./src/models/automoveis.json", JSON.stringify(automoveis), 'utf8', function (err) { // gravo meu json de automoveis atualizado
+            if (err) {
+                res.status(500).send({ message: err })
+            } else {
+                console.log("Arquivo atualizado com sucesso!")
+                const automoveisUpdated = automoveis.find((automoveis) => automoveis.id == automoveisId) // separo o automovel que modifiquei no array
+                res.status(200).send(automoveisUpdated) // envio o automovel modificado como resposta
+            }
+        })
+    } catch (err) {
+        res.status(500).send({ message: err })
+
+    } 
+}
+const deleteAutomoveis = (req, res) => {
+    try {
+        const automoveisId = req.params.id
+        const automoveisFound = automoveis.find(automoveis => automoveis.id == automoveisId) // encontro o automovel pelo id
+        const automoveisIndex = automoveis.indexOf(automoveisFound) // identifico o índice do automovel no meu array
+
+        if (automoveisIndex >= 0) { // verifico se o automovel existe no array de automoveis
+            automoveis.splice(automoveisIndex, 1) // removo o automovel pelo índice
+        } else {
+            res.status(404).send({ message: "Automovel não encontrado para ser deletado" })
+        }
+
+        fs.writeFile("./src/models/automoveis.json", JSON.stringify(automoveis), 'utf8', function (err) { // gravo meu array de filmes sem o filme que deletei
+            if (err) {
+                res.status(500).send({ message: err })
+            } else {
+                console.log("automovel deletado com sucesso do arquivo!")
+                res.sendStatus(204)
+            }
+        })
+    } catch (err) {
+        console.log(err)
+        res.status(500).send({ message: "Erro ao deletar o automovel" })
+    }
+}
+
 module.exports = {
     updateAutomoveis,
+    updatenovoStatus,
     getAutomoveis,
     getAutomoveisNome,
     createAutomoveis,
-    getAllAutomoveis,
+    deleteAutomoveis,
+    getAllAutomoveis
 }
--- src/routes/automoveisRoutes.js
+++ src/routes/automoveisRoutes.js
@@ -6,7 +6,9 @@ router.get("/", controller.getAllAutomoveis);
 router.post("/", controller.createAutomoveis);
 router.get("/:id", controller.getAutomoveis);
 router.get("/:carro", controller.getAutomoveisNome);
-router.put("/:id", controller.updateAutomoveis)
+router.put("/:id", controller.updateAutomoveis);
+router.patch("/:id/novo", controller.updatenovoStatus);
+router.delete("/:id", controller.deleteAutomoveis)
 
 
 module.exports = router;
